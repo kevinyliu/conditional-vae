fixed log softmax + used PyTorch 0.4 tools for KL and reparameterization + added .gitignore

cvae.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+from torch.distributions import Normal
+from torch.distributions.kl import kl_divergence
+
 from encoder import Encoder
 from decoder import BasicDecoder
 from inferer import Prior, ApproximatePosterior
@@ -21,8 +24,8 @@ class CVAE(nn.Module):
             eps = eps.cuda()
         return mu + eps * torch.exp(log_var/2)
 
-    def step():
-        pass
+    # def step():
+    #     pass
 
     def forward(self, src, trg):
         encoded_src = self.src_encoder(src)
@@ -31,8 +34,14 @@ class CVAE(nn.Module):
         mu_prior, log_var_prior = self.p(encoded_src)
         mu_posterior, log_var_posterior = self.q(encoded_src, encoded_trg)
 
-        z = self.reparameterize(mu_posterior, log_var_posterior)
+        p_normal = Normal(loc=mu_prior, scale=log_var_prior.mul(0.5).exp())
+        q_normal = Normal(loc=mu_posterior, scale=log_var_posterior.mul(0.5).exp())
+        kl = kl_divergence(q_normal, p_normal).sum()
+
+        # z = self.reparameterize(mu_posterior, log_var_posterior)
+        z = q_normal.rsample()
 
         ll, hidden = self.decoder(trg, z, encoded_src)
 
-        return ll, hidden, mu_prior, log_var_prior, mu_posterior, log_var_posterior
+        return ll, kl, hidden
+               # //mu_prior, log_var_prior, mu_posterior, log_var_posterior

decoder.py
@@ -19,5 +19,5 @@ class BasicDecoder(nn.Module):
         x = torch.cat((x, z.unsqueeze(0).repeat(trg.size(0),1,1)), dim=2)
         output, hidden = self.lstm(x, hidden)
         output = self.dropout(output)
-        output = F.softmax(self.linear(output), dim=2)
+        output = F.log_softmax(self.linear(output), dim=2)
         return output, hidden

train.py
@@ -18,16 +18,16 @@ def train(model, model_name, train_iter, val_iter, SRC_TEXT, TRG_TEXT, num_epoch
         for batch in tqdm(train_iter):
             src, trg = (batch.src.cuda(), batch.trg.cuda()) if gpu else (batch.src, batch.trg)
 
-            ll, hidden, mu_prior, log_var_prior, mu_posterior, log_var_posterior = model(src, trg)
+            ll, kl, hidden = model(src, trg)
 
             nll = loss(ll[:-1, :, :].view(-1, ll.size(2)), trg[1:, :].view(-1))
             
-            kl = torch.sum(0.5 * (((mu_prior - mu_posterior)**2 + torch.exp(log_var_posterior)) / torch.exp(log_var_prior) + (log_var_prior - log_var_posterior) - 1))
+            # kl = torch.sum(0.5 * (((mu_prior - mu_posterior)**2 + torch.exp(log_var_posterior)) / torch.exp(log_var_prior) + (log_var_prior - log_var_posterior) - 1))
             
             neg_elbo = nll + kl
 
-            train_nll += nll.data
-            train_kl += kl.data
+            train_nll += nll.item()
+            train_kl += kl.item()
 
             optimizer.zero_grad()
             neg_elbo.backward()
@@ -40,7 +40,7 @@ def train(model, model_name, train_iter, val_iter, SRC_TEXT, TRG_TEXT, num_epoch
         results = 'Epoch: {} Loss: {:.4f} NLL: {:.4f} KL: {:.4f}'.format(epoch+1, train_loss, train_nll, train_kl)
         print(results)
 
-        if not (epoch + 1) % 10:
+        if not (epoch + 1) % 1:
             local_path = os.getcwd()
             model_path = local_path + "/" + model_name
             if not os.path.exists(model_path):