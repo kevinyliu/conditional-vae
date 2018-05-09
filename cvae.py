correct implementation of word dropout (only masking input for seq2seq part of model)

cvae.py
@@ -9,7 +9,7 @@ import decoder
 import inferer
 
 class CVAE(nn.Module):
-    def __init__(self, src_vocab_size, trg_vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt=0.3, word_dpt=0.0, share_params=False):
+    def __init__(self, src_vocab_size, trg_vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt=0.3, share_params=False):
         super(CVAE, self).__init__()
         self.if_zero = False
         self.share_params = share_params
@@ -27,13 +27,12 @@ class CVAE(nn.Module):
             self.trg_encoder = encoder.Encoder(trg_vocab_size, embed_size, hidden_size, num_layers, dpt, self.trg_embedding)
         
         #self.decoder = decoder.BasicDecoder(trg_vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt, word_dpt, self.trg_embedding)
-        self.decoder = decoder.BasicAttentionDecoder(trg_vocab_size, embed_size, 2 * hidden_size, latent_size, num_layers, dpt, word_dpt, self.trg_embedding)
+        self.decoder = decoder.BasicAttentionDecoder(trg_vocab_size, embed_size, 2 * hidden_size, latent_size, num_layers, dpt, self.trg_embedding)
         #self.decoder = decoder.DummyDecoder(trg_vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt, self.trg_embedding)
         #self.decoder = decoder.BahdanauAttnDecoder(trg_vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt, self.trg_embedding)
         
         self.p = inferer.Prior(hidden_size, latent_size, dpt)
 #         self.p = inferer.SelfAttentionPrior(hidden_size, latent_size, dpt)
-        #self.q = inferer.ApproximatePosterior(hidden_size, latent_size, dpt)
         #self.q = inferer.ApproximatePosterior(hidden_size, latent_size, dpt)
         self.q = inferer.LSTMAttentionApproximatePosterior(hidden_size, latent_size, dpt)
 
@@ -50,12 +49,16 @@ class CVAE(nn.Module):
         ll, hidden = self.decoder(trg, z, encoded_src_t, hidden)
         return ll, hidden
 
-    def forward(self, src, trg):
+    def forward(self, src, trg, word_dpt=0.0):
+        # perform masking for translation encoder and decoder to weaken the language model
+        src_dpt_mask = torch.bernoulli((1 - word_dpt) * torch.ones(src.size())).type_as(src)
+        trg_dpt_mask = torch.bernoulli((1 - word_dpt) * torch.ones(trg.size())).type_as(trg)
         if self.share_params:
-            encoded_src_t = encoded_src_i = self.shared_encoder(self.src_embedding(src))
+            encoded_src_t = self.shared_encoder(self.src_embedding(src * src_dpt_mask))
+            encoded_src_i = self.shared_encoder(self.src_embedding(src))
             encoded_trg = self.shared_encoder(self.trg_embedding(trg))
         else:
-            encoded_src_t = self.src_encoder_t(src)
+            encoded_src_t = self.src_encoder_t(src * src_dpt_mask)
             encoded_src_i = self.src_encoder_i(src)
             encoded_trg = self.trg_encoder(trg)
 
@@ -68,7 +71,7 @@ class CVAE(nn.Module):
 
         z = q_normal.rsample()
 
-        ll, hidden = self.decoder(trg, z, encoded_src_t)
+        ll, hidden = self.decoder(trg * trg_dpt_mask, z, encoded_src_t)
 
         return ll, kl, hidden, mu_prior, log_var_prior, mu_posterior, log_var_posterior
 

decoder.py
@@ -40,7 +40,7 @@ class BasicDecoder(nn.Module):
         return output, hidden
 
 class BasicAttentionDecoder(nn.Module):
-    def __init__(self, vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt=0.3, word_dpt=0.0, embedding=None):
+    def __init__(self, vocab_size, embed_size, hidden_size, latent_size, num_layers, dpt=0.3, embedding=None):
         super(BasicAttentionDecoder, self).__init__()
         
         if embedding is not None:
@@ -48,8 +48,6 @@ class BasicAttentionDecoder(nn.Module):
         else:
             self.embedding = nn.Embedding(vocab_size, embed_size)
             self.embedding.weight.data.copy_((torch.rand(vocab_size, embed_size) - 0.5) * 2)
-        
-        self.word_dpt = word_dpt
 
         # projection of latent variable
         #self.linear_z = nn.Linear(latent_size, hidden_size)  
@@ -65,18 +63,12 @@ class BasicAttentionDecoder(nn.Module):
         # weight tying
         self.linear2.weight = self.embedding.weight
         
-    def forward(self, trg, z, encoded_src, hidden=None, word_dpt=0.0):
+    def forward(self, trg, z, encoded_src, hidden=None):
         trg_len = trg.size(0)
         batch_size = trg.size(1)
 
         x = self.embedding(trg)
         x = self.dropout(x)
-
-        # word dropout
-        mask = torch.bernoulli((1 - self.word_dpt) * torch.ones(trg_len, batch_size)).unsqueeze(2).expand_as(x)
-        if x.is_cuda:
-            mask = mask.cuda()
-        x = x * mask
 
         # projection of latent variable
         #h_z = F.tanh(self.linear_z(z))

train.py
@@ -29,16 +29,12 @@ def train(model, model_name, train_iter, val_iter, SRC_TEXT, TRG_TEXT, anneal, n
         
         for batch in tqdm(train_iter):
             # note: word dropout masking works this way because the '<unk>' tokens happen to be 0 in both languages
-            src_dpt_mask = torch.bernoulli((1 - word_dpt) * torch.ones(batch.src.size())).type(torch.LongTensor)
-            trg_dpt_mask = torch.bernoulli((1 - word_dpt) * torch.ones(batch.trg.size())).type(torch.LongTensor)
-            src_dpt_mask = src_dpt_mask.cuda() if gpu else src_dpt_mask
-            trg_dpt_mask = trg_dpt_mask.cuda() if gpu else trg_dpt_mask
-            src = batch.src * src_dpt_mask
-            trg = batch.trg * trg_dpt_mask
+            src = batch.src
+            trg = batch.trg
 
             trg_word_cnt = (trg != pad).float().sum() - trg.size(1)
             
-            re, kl, hidden, mu_prior, log_var_prior, mu_posterior, log_var_posterior = model(src, trg)
+            re, kl, hidden, mu_prior, log_var_prior, mu_posterior, log_var_posterior = model(src, trg, word_dpt)
             
             kl_word = kl.sum() / trg_word_cnt # KL by word
             kl_sent = kl.sum() / len(kl) # KL by sent
@@ -56,7 +52,7 @@ def train(model, model_name, train_iter, val_iter, SRC_TEXT, TRG_TEXT, anneal, n
 
             optimizer.zero_grad()
             neg_elbo.backward()
-            torch.nn.utils.clip_grad_norm(model.parameters(), 1.0)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             optimizer.step()
                     
         train_nre /= len(train_iter)